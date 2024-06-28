ffa4ee44d4c1d67f4f760527b3e4a2dcd80ed114
Aleksey Smakhtin <lokomotiv96000@gmail.com>
2024-06-29 02:31:01 +0300

    first

--- GameClient/src/main.tsx
+++ GameClient/src/main.tsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {ChakraProvider} from "@chakra-ui/react";
+import {I18nextProvider} from "react-i18next";
+import i18n from './i18n';
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ChakraProvider>
+            <I18nextProvider i18n={i18n}>
+                <BrowserRouter>
+                    <App/>
+                </BrowserRouter>
+            </I18nextProvider>
+        </ChakraProvider>
+    </React.StrictMode>,
 )
